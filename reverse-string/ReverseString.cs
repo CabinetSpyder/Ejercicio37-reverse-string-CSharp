4b65905b41060dc9567296eaae6bf11ba13e41eb
¨CabinetSpyder¨ <andres.artaloytia@gmail.com>
2025-05-09 11:03:49 +0200

    primera iteracion de la solucion

--- reverse-string/ReverseString.cs
+++ reverse-string/ReverseString.cs
@@ -1,7 +1,23 @@
+using System.Text;
+
 public static class ReverseString
 {
     public static string Reverse(string input)
     {
-        throw new NotImplementedException("You need to implement this method.");
+        if(string.IsNullOrEmpty(input))
+        {
+            return "";
+        }
+
+        //return new string(input.Reverse().ToArray());
+        var reversed = new StringBuilder();
+
+        for(int i = input.Length-1; i>=0; i--)
+        {
+            reversed.Append(input[i]);
+        }
+
+        return reversed.ToString();
+
     }
 }
